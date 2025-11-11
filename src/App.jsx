ba1bb3cbd5d6d9ecc27e0798c12974b6739c5297
PadmavathiGangavaram <lakshmipadmavathigangavaram@gmail.com>
2025-11-11 18:59:31 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Button from "./Button";
 export default function App() {
   return (
     <div>
+      <h1> hii, hello welcome to react - redux</h1>
       <UserInfo/>
       <Button/>
     </div>
